1b30e1b215bb2c5090be3f2e35bf3ff5e0c704c7
Torben Rasmussen <rtorben@gmail.com>
2014-03-06 09:48:24 -0800

    turn random-graph into a function

--- src/rhizome_fun/core.clj
+++ src/rhizome_fun/core.clj
@@ -17,10 +17,6 @@
         [a b] (split-at n nodes)]
     [pick (vec (concat a (drop 1 b)))]))
 
-(comment
-  (random-from alpha-nodes)
-  (pick-nodes alpha-nodes))
-
 (defn pick-nodes [nodes]
   (loop [picks [], nodes nodes, n (rand-int (count nodes))]
     (cond (zero? n) picks
@@ -28,57 +24,52 @@
           (let [[pick remaining] (random-from nodes)]
             (recur (conj picks pick) remaining (dec n))))))
 
-(comment
-  (require '[clojure.pprint :refer [pprint]])
-  (pprint (make-graph (pick-nodes alpha-nodes-lower))))
-
 (defn make-graph [nodes]
   (for [node nodes
         :let [picks (pick-nodes nodes)]]
     [node picks]))
 
-(let [nodes (pick-nodes (vec (take 10 alpha-nodes)))
-      ; TODO analyze word adjacency
-      g (into {} (make-graph nodes))
-      cluster (fn [n] (rand-int (count nodes)))
-      cluster (fn [n] (cond ((set alpha-nodes-vowel) n) :vowel
-                            ((set [:b :c :d :f :B :C :D :F]) n) :bcdf
-                            :else :other))
-      dot
-      (graph->dot
-        (keys g)
-        g
-        :node->descriptor (fn [n] {:label n})
-        ;; TODO node options allow for assigning rank?
-        :node->cluster cluster
-        :cluster->descriptor {
-                              :vowel {:label :vowel :rank :same}
-                              :bcdf  {:label :bcdf  :rank :same}
-                              :other {:label :other :rank :same}
-                              }
-        ; :edge->descriptor (fn [a b]
-        ;                     (condp = [a b]
-        ;                       [a :a] {:label b}
-        ;                       [a :b] {:label b}
-        ;                       [a :c] {:label b}
-        ;                       [a :d] {:label b}
-        ;                       [a :e] {:label b}
-        ;                       [a :f] {:label b}
-        ;                       [a :g] {:label b}
-        ;                       [a :h] {:label b}
-        ;                       [a :i] {:label b}
-        ;                       [a :j] {:label b}
-        ;                       [a :k] {:label b}
-        ;                       [a :l] {:label b}
-        ;                       [a :m] {:label b}
-        ;                       [a b] {:label b}
-        ;                       ))
-        :options {:rankdir :BT
-                  :dpi 300
-                  :node {:fontname "Arial"}
-                  :edge {:test "foo"}
-                  }
-        :vertical? false
-        :filename "test.png")
-      ]
-  (-> dot dot->image (save-image "test.png")))
+(defn random-graph [nodes & {:keys [node->cluster
+                                    cluster->descriptor]
+                             :or {node->cluster (fn [n] (rand-int (count nodes)))
+                                  cluster->descriptor (fn [n] {:label (str n)})}}]
+  (let [nodes (pick-nodes (vec nodes))
+        g (into {} (make-graph nodes))
+        dot
+        (graph->dot
+          (keys g)
+          g
+          :node->descriptor (fn [n] {:label n})
+          :node->cluster node->cluster
+          :cluster->descriptor cluster->descriptor
+          ; :edge->descriptor nil
+          :options {:rankdir :BT
+                    :dpi 100
+                    :node {:fontname "Arial"}}
+          :vertical? false
+          :filename "test.png")]
+    dot))
+
+(def view-dot (comp view-image dot->image))
+
+(comment
+  (random-from alpha-nodes)
+  (pick-nodes alpha-nodes)
+
+  (require '[clojure.pprint :refer [pprint]])
+  (pprint (make-graph (pick-nodes alpha-nodes-lower)))
+
+  ; FIXME deal with clusters containing a single cluster
+  (view-dot (random-graph (vec (range 26))))
+
+  (view-dot
+    (random-graph (take 20 alpha-nodes)
+                  :cluster (fn [n] (cond ((set alpha-nodes-vowel) n) :vowel
+                                         ((set [:b :c :d :f :B :C :D :F]) n) :bcdf
+                                         :else :other))
+                  :cluster->descriptor {:vowel {:label :vowel :rank :same}
+                                        :bcdf  {:label :bcdf  :rank :same}
+                                        :other {:label :other :rank :same}}))
+
+
+  )
